3b0a8441f499032eb7ff12f866480c6b21da3542
Hackxolotl <63atlas-surhausse@icloud.com>
2026-05-28 18:23:54 +0200

    Update destinations.php

--- backend/api/destinations.php
+++ backend/api/destinations.php
@@ -1,1 +1,12 @@
+<?php
 
+require_once "../db.php";
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+$stmt = $pdo->query("SELECT * FROM destinations");
+
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($data);
