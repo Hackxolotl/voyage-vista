91e7b4100ad646d98b45e9bc8a3ad65a208648ec
Hackxolotl <Hackxolotl@users.noreply.github.com>
2026-05-28 17:10:26 +0000

    deploy: 3e0ac0172cdd6099dad7920aeb92bc8e0b1574f3

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL =
+export const API_URL = "https://voyage-vista.up.railway.app";
   window.location.hostname === "localhost"
     ? "http://localhost:8000"
     : "https://TON-BACKEND.up.railway.app";
