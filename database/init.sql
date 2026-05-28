fc9647f83973ccefa00b62e8613867038d6c73b8
Hackxolotl <63atlas-surhausse@icloud.com>
2026-05-28 18:21:52 +0200

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,12 @@
+CREATE TABLE destinations (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  price INT
+);
 
+INSERT INTO destinations (name, description, price)
+VALUES
+('Paris', 'Ville lumière', 120),
+('Rome', 'Ville antique', 150),
+('Tokyo', 'Ville futuriste', 900);
