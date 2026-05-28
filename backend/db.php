1118fad329ca3f78745301a1994f331d42387009
Hackxolotl <63atlas-surhausse@icloud.com>
2026-05-28 18:22:06 +0200

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,1 +1,22 @@
+<?php
 
+$host = getenv("DB_HOST") ?: "db";
+$db   = getenv("DB_NAME") ?: "voyagevista";
+$user = getenv("DB_USER") ?: "user";
+$pass = getenv("DB_PASSWORD") ?: "password";
+
+try {
+
+  $pdo = new PDO(
+    "mysql:host=$host;dbname=$db;charset=utf8",
+    $user,
+    $pass
+  );
+
+} catch (Exception $e) {
+
+  die(json_encode([
+    "success" => false,
+    "error" => $e->getMessage()
+  ]));
+}
