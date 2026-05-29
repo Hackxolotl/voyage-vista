d5361126a5dccdb58f2202dddaa63f7c411cf029
Hackxolotl <63atlas-surhausse@icloud.com>
2026-05-29 03:00:13 +0200

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,24 +1,24 @@
 <?php
 
-$host = $_ENV["MYSQLHOST"];
-$port = $_ENV["MYSQLPORT"];
-$db   = $_ENV["MYSQLDATABASE"];
-$user = $_ENV["MYSQLUSER"];
-$pass = $_ENV["MYSQLPASSWORD"];
-
 try {
+
     $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
-        $user,
-        $pass
+        "mysql:host=" . $_ENV["MYSQLHOST"] .
+        ";port=" . $_ENV["MYSQLPORT"] .
+        ";dbname=" . $_ENV["MYSQLDATABASE"],
+        $_ENV["MYSQLUSER"],
+        $_ENV["MYSQLPASSWORD"]
     );
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo json_encode([
+        "success" => true,
+        "message" => "Database connected"
+    ]);
 
 } catch(PDOException $e) {
 
-    die(json_encode([
+    echo json_encode([
         "success" => false,
         "error" => $e->getMessage()
-    ]));
+    ]);
 }
