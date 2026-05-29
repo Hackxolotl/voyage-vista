9987df48b6b90bfe1d5a3232c29d3b4cfb6b3087
Hackxolotl <63atlas-surhausse@icloud.com>
2026-05-30 01:57:59 +0200

    Update packages.php

--- backend/api/packages.php
+++ backend/api/packages.php
@@ -1,10 +1,10 @@
 <?php
+require "db.php";
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
-require "../db.php";
-
 header("Content-Type: application/json");
 
 try {
